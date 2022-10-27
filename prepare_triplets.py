16e1cb0376571cf8858b75af64463ebfa12c5ca7
Marketa Krenkova <marketa@abiff.ics.muni.cz>
2022-10-27 13:51:07 +0200

    preparing triplets done (for now)

--- prepare_triplets.py
+++ prepare_triplets.py
@@ -9,24 +9,24 @@ import json
 drug_dir = '../data/drugbank/'
 triplets_dir = '../data/triplets/'
 
-# # drug_id - name
-# drugs = pd.read_csv(drug_dir + 'drug_id_name_map.csv', index_col=[0])
-# drugs['relation'] = list(itertools.repeat('has_name', drugs.shape[0]))
-# drugs = drugs.iloc[:,[0,2,1]]
-# print(drugs.head())
-# print()
-# drugs.to_csv(triplets_dir + 'drugs_names.tsv', sep='\t')
+# drug_id - name
+drugs = pd.read_csv(drug_dir + 'drug_id_name_map.csv', index_col=[0])
+drugs['relation'] = list(itertools.repeat('has_name', drugs.shape[0]))
+drugs = drugs.iloc[:,[0,2,1]]
+print(drugs.head())
+print()
+drugs.to_csv(triplets_dir + 'drugs_names.tsv', sep='\t')
 
-# # category - mesh_id ?
+# category - mesh_id ?
 
-# # drug_id - subclass
-# drug_class = pd.read_csv(drug_dir + 'drug_classification.csv', index_col=[0])
-# drug_subclass = drug_class[['id', 'subclass']]
-# drug_subclass['relation'] = list(itertools.repeat('in_subclass', drug_subclass.shape[0]))
-# drug_subclass = drug_subclass.iloc[:,[0,2,1]]
-# print(drug_subclass.head())
-# print()
-# drug_subclass.to_csv(triplets_dir + 'drug_subclass.tsv', sep='\t')
+# drug_id - subclass
+drug_class = pd.read_csv(drug_dir + 'drug_classification.csv', index_col=[0])
+drug_subclass = drug_class[['id', 'subclass']]
+drug_subclass['relation'] = list(itertools.repeat('in_subclass', drug_subclass.shape[0]))
+drug_subclass = drug_subclass.iloc[:,[0,2,1]]
+print(drug_subclass.head())
+print()
+drug_subclass.to_csv(triplets_dir + 'drug_subclass.tsv', sep='\t')
 
 # subclass - class ?
 # TODO: to lower case
@@ -44,71 +44,71 @@ triplets_dir = '../data/triplets/'
 # drug_id - interaction - food
 # TODO: parser 
 
-# # drug_name - ingredients
-# mixtures = pd.read_csv(drug_dir + 'drug_mixtures.csv', index_col=[0])
-# drugs = []
-# ingredients = []
-# relations = []
-# for drug in mixtures.itertuples():
-#     ingreds = drug[2].split('+')
-#     for ingredient in ingreds:
-#         drugs.append(drug[1])
-#         relations.append('contains')
-#         ingredients.append(ingredient)
-# ingredient_df = pd.DataFrame({'drug_name': drugs, 'relation': relations, 'ingredient': ingredients})        
-# print(ingredient_df.head())   
-# print()    
-# ingredient_df.to_csv(triplets_dir + 'ingredients.tsv', sep='\t')
+# drug_name - ingredients
+mixtures = pd.read_csv(drug_dir + 'drug_mixtures.csv', index_col=[0])
+drugs = []
+ingredients = []
+relations = []
+for drug in mixtures.itertuples():
+    ingreds = drug[2].split('+')
+    for ingredient in ingreds:
+        drugs.append(drug[1])
+        relations.append('contains')
+        ingredients.append(ingredient)
+ingredient_df = pd.DataFrame({'drug_name': drugs, 'relation': relations, 'ingredient': ingredients})        
+print(ingredient_df.head())   
+print()    
+ingredient_df.to_csv(triplets_dir + 'ingredients.tsv', sep='\t')
 
-# # drug_id - inchi_key
-# inchi_key = pd.read_csv(drug_dir + 'drug_inchi_key.csv', index_col=[0])
-# inchi_key['relation'] = list(itertools.repeat('has_inchi_key', inchi_key.shape[0]))
-# inchi_key = inchi_key.iloc[:,[0,2,1]]
-# print(inchi_key.head())
-# print()
-# inchi_key.to_csv(triplets_dir + 'drugs_inchi_key.tsv', sep='\t')
+# drug_id - inchi_key
+inchi_key = pd.read_csv(drug_dir + 'drug_inchi_key.csv', index_col=[0])
+inchi_key['relation'] = list(itertools.repeat('has_inchi_key', inchi_key.shape[0]))
+inchi_key = inchi_key.iloc[:,[0,2,1]]
+print(inchi_key.head())
+print()
+inchi_key.to_csv(triplets_dir + 'drugs_inchi_key.tsv', sep='\t')
 
-# # drug_id - molecule
-# molecules = pd.read_csv(drug_dir + 'drug_molecul.csv', index_col=[0])
-# molecules['relation'] = list(itertools.repeat('has_molecule', molecules.shape[0]))
-# molecules = molecules.iloc[:,[0,2,1]]
-# print(molecules.head())
-# print()
-# molecules.to_csv(triplets_dir + 'drugs_molecule.tsv', sep='\t')
+# drug_id - molecule
+molecules = pd.read_csv(drug_dir + 'drug_molecul.csv', index_col=[0])
+molecules['relation'] = list(itertools.repeat('has_molecule', molecules.shape[0]))
+molecules = molecules.iloc[:,[0,2,1]]
+print(molecules.head())
+print()
+molecules.to_csv(triplets_dir + 'drugs_molecule.tsv', sep='\t')
 
 
-# # salt_id - salt name
-# salts = pd.read_csv(drug_dir + 'drug_salts.csv', index_col=[0])
-# salts_name = salts[['id', 'name']]
-# salts_name['relation'] = list(itertools.repeat('has_name', salts_name.shape[0]))
-# salts_name = salts_name.iloc[:,[0,2,1]]
-# print(salts_name.head())
-# print()
-# salts_name.to_csv(triplets_dir + 'salts_name.tsv', sep='\t')
+# salt_id - salt name
+salts = pd.read_csv(drug_dir + 'drug_salts.csv', index_col=[0])
+salts_name = salts[['id', 'name']]
+salts_name['relation'] = list(itertools.repeat('has_name', salts_name.shape[0]))
+salts_name = salts_name.iloc[:,[0,2,1]]
+print(salts_name.head())
+print()
+salts_name.to_csv(triplets_dir + 'salts_name.tsv', sep='\t')
 
-# # drug_id - salt_id
-# drug_slats = salts[['id', 'drug']]
-# drug_slats['relation'] = list(itertools.repeat('contains_salt', drug_slats.shape[0]))
-# drug_slats = drug_slats.iloc[:,[1,2,0]]
-# print(drug_slats.head())
-# print()
-# drug_slats.to_csv(triplets_dir + 'drug_slats.tsv', sep='\t')
+# drug_id - salt_id
+drug_slats = salts[['id', 'drug']]
+drug_slats['relation'] = list(itertools.repeat('contains_salt', drug_slats.shape[0]))
+drug_slats = drug_slats.iloc[:,[1,2,0]]
+print(drug_slats.head())
+print()
+drug_slats.to_csv(triplets_dir + 'drug_slats.tsv', sep='\t')
 
-# # salt_id - cas_number
-# salts_cas_numebr = salts[['id', 'cas_number']].dropna()
-# salts_cas_numebr['relation'] = list(itertools.repeat('has_cas_number', salts_cas_numebr.shape[0]))
-# salts_cas_numebr = salts_cas_numebr.iloc[:,[0,2,1]]
-# print(salts_cas_numebr.head())
-# print()
-# salts_cas_numebr.to_csv(triplets_dir + 'salts_cas_numebr.tsv', sep='\t')
+# salt_id - cas_number
+salts_cas_numebr = salts[['id', 'cas_number']].dropna()
+salts_cas_numebr['relation'] = list(itertools.repeat('has_cas_number', salts_cas_numebr.shape[0]))
+salts_cas_numebr = salts_cas_numebr.iloc[:,[0,2,1]]
+print(salts_cas_numebr.head())
+print()
+salts_cas_numebr.to_csv(triplets_dir + 'salts_cas_numebr.tsv', sep='\t')
 
-# # salt_id - inchi_key
-# salts_inchi = salts[['id', 'inchi_key']].dropna()
-# salts_inchi['relation'] = list(itertools.repeat('has_inchi_key', salts_inchi.shape[0]))
-# salts_inchi = salts_inchi.iloc[:,[0,2,1]]
-# print(salts_inchi.head())
-# print()
-# salts_inchi.to_csv(triplets_dir + 'salts_salts_inchi_key.tsv', sep='\t')
+# salt_id - inchi_key
+salts_inchi = salts[['id', 'inchi_key']].dropna()
+salts_inchi['relation'] = list(itertools.repeat('has_inchi_key', salts_inchi.shape[0]))
+salts_inchi = salts_inchi.iloc[:,[0,2,1]]
+print(salts_inchi.head())
+print()
+salts_inchi.to_csv(triplets_dir + 'salts_salts_inchi_key.tsv', sep='\t')
 
 # -----------------------------------------------------------
 # FooDB
@@ -160,14 +160,68 @@ compound_cas_numbers_df.to_csv(triplets_dir + 'compounds_cas_number.tsv', sep='\
 
 # -----------------------------------------------------------
 # iDISK
+ds_data_dir = '../data/idisk-rrf/'
 
 # concept id - type
-# atom id - name
-# atom id - source db
-# atom id - id in source db
+concept_type = pd.read_csv(ds_data_dir + 'MRSTY.csv', sep='|')
+concept_type['relation'] = list(itertools.repeat('is_type', concept_type.shape[0]))
+concept_type = concept_type.iloc[:,[0,2,1]]
+print(concept_type.head())
+print()
+concept_type.to_csv(triplets_dir + 'ds_concept_type.tsv', sep='\t')
+
+# atom id - concept id
+atoms = pd.read_csv(ds_data_dir + 'MRCONSO.csv', sep='|')
+atoms_concept = atoms[['CUI', 'AUI']]
+atoms_concept['relation'] = list(itertools.repeat('maps_to', atoms_concept.shape[0]))
+atoms_concept = atoms_concept.iloc[:,[0,2,1]]
+print(atoms_concept.head())
+print()
+atoms_concept.to_csv(triplets_dir + 'ds_atoms_concept_map.tsv', sep='\t')
+
+# atom id -name
+atoms_name = atoms[['AUI', 'STR']]
+atoms_name['relation'] = list(itertools.repeat('has_name', atoms_name.shape[0]))
+atoms_name = atoms_name.iloc[:,[0,2,1]]
+print(atoms_name.head())
+print()
+atoms_name.to_csv(triplets_dir + 'ds_atoms_names.tsv', sep='\t')
+
+# atom id - source db ?
+# atom id - id in source db ?
+
+# TODO: parser
 # attribute id - attribute name
-# attribute id - type (concept/relation)
-# attribute id - concept/relation id
+# attributes = pd.read_csv(ds_data_dir + 'MRSAT.csv', sep='|')
+# attr_name = attributes[['ATUI', 'ATN']]
+# attr_name['relation'] = list(itertools.repeat('has_name', attr_name.shape[0]))
+# attr_name = attr_name.iloc[:,[0,2,1]]
+# print(attr_name.head())
+# print()
+# attr_name.to_csv(triplets_dir + 'ds_attribute_names.tsv', sep='\t')
+
+# # attribute id - type (concept/relation) ?
+
+# # attribute id - concept/relation id
+# attr_id_map = attributes[['ATUI', 'UI']]
+# attr_id_map['relation'] = list(itertools.repeat('maps_to', attr_id_map.shape[0]))
+# attr_id_map = attr_id_map.iloc[:,[0,2,1]]
+# print(attr_id_map.head())
+# print()
+# attr_id_map.to_csv(triplets_dir + 'ds_attr_id_map.tsv', sep='\t')
+
 # attribute id - value (decription)
+# attr_val = attributes[['ATUI', 'UI']]
+# attr_val['relation'] = list(itertools.repeat('descritpion', attr_val.shape[0]))
+# attr_val = attr_val.iloc[:,[0,2,1]]
+# print(attr_val.head())
+# print()
+# attr_val.to_csv(triplets_dir + 'ds_attr_description.tsv', sep='\t')
+
 # relation: concept1 id - relation id - concept2 id
+relations = pd.read_csv(ds_data_dir + 'MRREL.csv', sep='|')
+relations = relations[['CUI1', 'REL', 'CUI2']]
+print(relations.head())
+relations.to_csv(triplets_dir + 'ds_relations,tsv', sep='\t')
+
 # relation id - description (TODO: create relation id + description map)
