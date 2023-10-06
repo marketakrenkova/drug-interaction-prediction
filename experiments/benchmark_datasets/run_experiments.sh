a5601ca3cc171c856d51f0d5b342180f525493f5
Markéta Křenková <xkrenkov@fi.muni.cz>
2023-10-06 13:06:25 +0200

    benchamrk datasets experiments

--- experiments/benchmark_datasets/run_experiments.sh
+++ experiments/benchmark_datasets/run_experiments.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-cd BioKG
-pykeen experiments ablation config.json -d results
-cd ..
+# cd BioKG
+# pykeen experiments ablation config.json -d results
+# cd ..
 
-cd Hetionet
-pykeen experiments ablation config.json -d results
-cd ..
+# cd Hetionet
+# pykeen experiments ablation config.json -d results
+# cd ..
 
 # cd OGBBioKG
 # pykeen experiments ablation config.json -d results
 # cd ..
 
-cd openBioLink
-pykeen experiments ablation config.json -d results
-cd ..
+# cd openBioLink
+# pykeen experiments ablation config.json -d results
+# cd ..
 
-cd PharmKG
+# cd PharmKG
+# pykeen experiments ablation config.json -d results
+# cd ..
+
+# cd wn18
+# pykeen experiments ablation config.json -d results
+# cd ..
+
+cd fb15k
 pykeen experiments ablation config.json -d results
 cd ..
---
+++ models/pykeen_benchmark_datasets.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+import wandb
+import os
+import torch
+from pykeen.pipeline import pipeline
+import pandas as pd
+
+os.environ["WANDB_API_KEY"] = "a0dcca4cf18920b5c23ec09023f46ffa76caad5b"
+wandb.login()
+
+dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def run(params):
+    pipeline_result = pipeline(
+        model=params['model'],
+        dataset=params['dataset'],
+        model_kwargs=dict(
+        embedding_dim=params['embedding_dim'],
+        ),
+        training_kwargs=dict(
+            num_epochs=50
+        ),
+        device=dev,
+        loss=params['loss'],
+        optimizer='Adam',
+        optimizer_kwargs=dict(
+            lr=params['lr'],
+        ),
+        negative_sampler_kwargs = dict(
+                num_negs_per_pos = params['num_negs_per_pos'],
+        ),
+        result_tracker='wandb',
+        result_tracker_kwargs=dict(
+            project='KG-benchmarks',
+        ),
+    )
+
+    mrr = pipeline_result.get_metric('mrr')
+    # print("MRR:", mrr)
+    hits10 = pipeline_result.get_metric('hits@10')
+    # print("Hits@10", hits10)
+
+    return mrr, hits10
+
+
+settings = [
+    {
+        'dataset': 'fb15k',
+        'model': 'complex',
+        'loss': 'bcewithlogits',
+        'embedding_dim': 112,
+        'num_negs_per_pos': 3,
+        'lr': 0.08
+    },
+    {
+        'dataset': 'fb15k',
+        'model': 'rotate',
+        'loss': 'marginranking',
+        'embedding_dim': 272,
+        'num_negs_per_pos': 24,
+        'lr': 0.01
+    },
+    {
+        'dataset': 'wn18',
+        'model': 'complex',
+        'loss': 'marginranking',
+        'embedding_dim': 192,
+        'num_negs_per_pos': 4,
+        'lr': 0.05
+    },
+    {
+        'dataset': 'wn18',
+        'model': 'rotate',
+        'loss': 'marginranking',
+        'embedding_dim': 528,
+        'num_negs_per_pos': 1,
+        'lr': 0.05
+    },
+]
+
+df_results = pd.DataFrame(columns=["dataset", "model", "mrr", "hits@10"])
+for i, s in enumerate(settings):
+    mrr, hits10 = run(s)
+    df_results.loc[i] = [s['dataset'], s['model'], mrr, hits10]
+
+print(df_results)
