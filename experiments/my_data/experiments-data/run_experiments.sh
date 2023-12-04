1f80e73c4851e8f83b1568b4784fd0cf32a411b7
Markéta Křenková <xkrenkov@fi.muni.cz>
2023-12-04 21:45:27 +0100

    new experiments

---
+++ experiments/my_data/experiments-data/run_experiments.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+#cd experiment-drugbank
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+#cd experiment-biokg
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+#cd experiment-hetionet
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+cd experiment-interactions
+pykeen experiments ablation config.json -d results
+cd ..
+
+# cd experiment-compounds
+# pykeen experiments ablation config.json -d results
+# cd ..
