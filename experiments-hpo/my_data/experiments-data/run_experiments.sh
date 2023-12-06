66cded75aacb8776bfeaa7c77f298be2e313e8a0
Markéta Křenková <xkrenkov@fi.muni.cz>
2023-12-06 15:33:35 +0100

    rename experiments

---
+++ experiments-hpo/my_data/experiments-data/run_experiments.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+#cd experiment-drugbank
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+#cd experiment-biokg
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+#cd experiment-hetionet
+#pykeen experiments ablation config.json -d results
+#cd ..
+
+cd experiment-interactions
+pykeen experiments ablation config.json -d results
+cd ..
+
